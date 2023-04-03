3c9d978f055ee23e0737eb35568faf5ada6f4176
JhonataCustodio <silvajhonata@live.com>
2023-04-03 21:45:37 +0044

    removing dependency JPA and mapping class Order and Items

--- ms-order/src/main/java/br/com/compassuol/pb/msorder/domain/entity/Order.java
+++ ms-order/src/main/java/br/com/compassuol/pb/msorder/domain/entity/Order.java
@@ -1,7 +1,22 @@
 package br.com.compassuol.pb.msorder.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Document(collection = "order")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+    @Id
     private Integer id;
+    @Field("cpf")
     private String cpf;
     private Items items;
     private Double amount;
