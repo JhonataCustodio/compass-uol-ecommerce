17c51f27b8ab874bb769050aa5c0e69029eba2c5
JhonataCustodio <silvajhonata@live.com>
2023-04-03 21:46:11 +0044

    mapping class Items

--- ms-order/src/main/java/br/com/compassuol/pb/msorder/domain/entity/Items.java
+++ ms-order/src/main/java/br/com/compassuol/pb/msorder/domain/entity/Items.java
@@ -1,8 +1,21 @@
 package br.com.compassuol.pb.msorder.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDate;
 
+@Document(collation = "items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Items {
+    @Id
     private Integer id;
     private String name;
     private String description;
