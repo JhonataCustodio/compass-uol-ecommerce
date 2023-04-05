26ca63fda0bd377403d7e2d0c0417bd0803ee0cb
JhonataCustodio <silvajhonata@live.com>
2023-04-05 21:06:45 +0044

    refactor configuration database

--- ms-order/src/main/java/br/com/compassuol/pb/msorder/MsOrderApplication.java
+++ ms-order/src/main/java/br/com/compassuol/pb/msorder/MsOrderApplication.java
@@ -2,11 +2,13 @@ package br.com.compassuol.pb.msorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MsOrderApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(MsOrderApplication.class, args);
 	}
 
