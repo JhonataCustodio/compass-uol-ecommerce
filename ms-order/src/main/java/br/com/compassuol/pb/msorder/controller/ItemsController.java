dbe4aecdc8abd2fad62226b1ef205fabadbc7afd
JhonataCustodio <silvajhonata@live.com>
2023-04-08 00:51:53 +0044

    implements method update items in controller and service

--- ms-order/src/main/java/br/com/compassuol/pb/msorder/controller/ItemsController.java
+++ ms-order/src/main/java/br/com/compassuol/pb/msorder/controller/ItemsController.java
@@ -3,11 +3,10 @@ package br.com.compassuol.pb.msorder.controller;
 import br.com.compassuol.pb.msorder.domain.dto.request.ItemsDtoRequest;
 import br.com.compassuol.pb.msorder.domain.dto.response.ItemsDtoResponse;
 import br.com.compassuol.pb.msorder.domain.service.ItemsService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ItemsController {
@@ -17,5 +16,11 @@ public class ItemsController {
     public ResponseEntity<ItemsDtoResponse> save(@RequestBody ItemsDtoRequest request){
         ItemsDtoResponse itemsDtoResponse = itemsService.save(request);
         return  ResponseEntity.ok(itemsDtoResponse);
+    }
+    @PutMapping("/api/order/{idOrder}/item/{idItem}")
+    public ResponseEntity<ItemsDtoResponse> update(@PathVariable("idOrder") ObjectId idOrder, @PathVariable("idItem")
+                       ObjectId idItem, @RequestBody ItemsDtoRequest request){
+        ItemsDtoResponse itemsDtoResponse = itemsService.update(idOrder, idItem, request);
+        return ResponseEntity.ok(itemsDtoResponse);
     }
 }
