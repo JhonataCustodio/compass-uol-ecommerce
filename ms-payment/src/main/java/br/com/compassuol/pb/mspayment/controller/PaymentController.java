f49fb3c93a53624ad781a44e263167bed665e194
JhonataCustodio <silvajhonata@live.com>
2023-04-08 14:25:25 +0044

    deleted save method for testing and implemented get method, in service and controller

--- ms-payment/src/main/java/br/com/compassuol/pb/mspayment/controller/PaymentController.java
+++ ms-payment/src/main/java/br/com/compassuol/pb/mspayment/controller/PaymentController.java
@@ -5,17 +5,20 @@ import br.com.compassuol.pb.mspayment.domain.dto.PaymentDtoResponse;
 import br.com.compassuol.pb.mspayment.domain.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
-    @PostMapping("/payment")
-    public ResponseEntity<PaymentDtoResponse> save(@RequestBody PaymentDtoRequest request){
-        PaymentDtoResponse paymentDtoResponse = paymentService.save(request);
+    @GetMapping("/api/payment")
+    public ResponseEntity<List<PaymentDtoResponse>> getAll(){
+        List<PaymentDtoResponse> paymentDtoResponse = paymentService.getAll();
         return ResponseEntity.ok(paymentDtoResponse);
     }
 }
--- ms-payment/src/main/java/br/com/compassuol/pb/mspayment/domain/service/PaymentService.java
+++ ms-payment/src/main/java/br/com/compassuol/pb/mspayment/domain/service/PaymentService.java
@@ -8,16 +8,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PaymentService {
     @Autowired
     private ModelMapper modelMapper;
     @Autowired
     private PaymentRepository paymentRepository;
-    public PaymentDtoResponse save(PaymentDtoRequest request){
-        Payment payment = modelMapper.map(request, Payment.class);
-        paymentRepository.save(payment);
-        PaymentDtoResponse paymentDtoResponse = modelMapper.map(payment, PaymentDtoResponse.class);
-        return  paymentDtoResponse;
+    public List<PaymentDtoResponse> getAll(){
+        List<Payment> payments = paymentRepository.findAll();
+        List<PaymentDtoResponse> paymentDtoResponses = payments.stream()
+                .map(payment -> modelMapper.map(payment, PaymentDtoResponse.class)).collect(Collectors.toList());
+        return  paymentDtoResponses;
     }
 }
