3b879ac41aaed725554ee9e5ff47531ee86fd650
JhonataCustodio <silvajhonata@live.com>
2023-04-09 20:35:48 +0044

    removal of imports that were not being used

--- ms-order/src/main/java/br/com/compassuol/pb/msorder/config/MessageConfig.java
+++ ms-order/src/main/java/br/com/compassuol/pb/msorder/config/MessageConfig.java
@@ -1,16 +1,11 @@
 package br.com.compassuol.pb.msorder.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
 
 @Configuration
 public class MessageConfig implements WebMvcConfigurer {
