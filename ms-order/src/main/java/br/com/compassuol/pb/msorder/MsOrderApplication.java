a630285747af7ffe8dd743b7fea550b68c09aeeb
JhonataCustodio <silvajhonata@live.com>
2023-04-10 16:50:08 +0044

    update main

--- ms-order/src/main/java/br/com/compassuol/pb/msorder/MsOrderApplication.java
+++ ms-order/src/main/java/br/com/compassuol/pb/msorder/MsOrderApplication.java
@@ -1,10 +1,12 @@
 package br.com.compassuol.pb.msorder;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableRabbit
 public class MsOrderApplication {
 
     public static void main(String[] args) {
