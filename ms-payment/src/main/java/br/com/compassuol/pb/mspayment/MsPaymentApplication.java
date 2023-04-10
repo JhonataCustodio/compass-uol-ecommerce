08cc0d011a693ebc617aea1398f60c84c12abcdb
JhonataCustodio <silvajhonata@live.com>
2023-04-10 16:46:55 +0044

    update main

--- ms-payment/src/main/java/br/com/compassuol/pb/mspayment/MsPaymentApplication.java
+++ ms-payment/src/main/java/br/com/compassuol/pb/mspayment/MsPaymentApplication.java
@@ -1,9 +1,11 @@
 package br.com.compassuol.pb.mspayment;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class MsPaymentApplication {
 
 	public static void main(String[] args) {
